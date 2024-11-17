38a26c9412dde6098e4eeecac8b939a0156c18fa
VladPashkovsky <v.v.pashkovsky@gmail.com>
2024-11-17 15:48:55 +0100

    start

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+
+const PORT = process.env.PORT || 8000
+
+const app = express();
+
+app.use(cors({
+  credentials: true,
+  origin: [
+    process.env.BASE_URL,
+    process.env.CLIENT_URL,
+    process.env.CLIENT_URL_PREVIEW,
+    process.env.CLIENT_LOCALHOST,
+    process.env.DATABASE_URL,
+    process.env.WD_HTTP,
+    process.env.WD_HTTPS,
+    process.env.WD_HTTP_PORT,
+    process.env.WD_HTTPS_PORT,
+    process.env.VITE_SERVER_ONE,
+    process.env.VITE_SERVER_TWO,
+  ],
+}))
+
+app.get('/', (req, res) => {
+  res.send('Hello SERVER Comments');
+});
+
+const start = async (req, res) => {
+  try {
+    // await client.connect()
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}`)
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
